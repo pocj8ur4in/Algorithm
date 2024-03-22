deddd18d8c67343753feea68b4f2004fb2d20600
pocj8ur4in <pocj8ur4in@gmail.com>
2024-03-22 11:40:41 +0900

    240322 commit
    # BOJ 15651. N과 M (3)

---
+++ BOJ/15651. N과 M (3).py
@@ -0,0 +1,26 @@
+import sys
+read = sys.stdin.readline
+
+
+def bt(ars, cnt):
+    if cnt == M:
+        arr.append(ars)
+    else:
+        for i in range(1, N + 1):
+            tmp = ars.copy()
+            tmp.append(i)
+            bt(tmp, cnt + 1)
+
+
+N, M = map(int, read().split())
+arr = []
+
+bt([], 0)
+
+for i in arr:
+    result = ""
+
+    for j in i:
+        result = result + str(j) + " "
+
+    print(result)
